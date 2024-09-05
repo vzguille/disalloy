84bdee960633beedbcaefdec36d54f0f018d88db
vzguille <vazqueztguillermo@gmail.com>
2024-09-05 08:26:33 +0000

    test qw added

--- disalloy/cellfunc.py
+++ disalloy/cellfunc.py
@@ -2,15 +2,18 @@ import numpy as np
 import ase
 
 
+def rnd_ball(dimension=3):
+    # Generate a vector with normally distributed components
+    v = np.random.normal(0, 1, dimension)
+    # Normalize the vector to make it a unit vector
+    v /= np.linalg.norm(v)
+    # Scale factor to ensure uniform distribution in the sphere
+    r = np.random.uniform(0, 1) ** (1.0 / dimension)
+    # Scale the unit vector to a random point within the unit sphere
+    return v * r 
+    
 
-def rnd_ball(dimension = 3):
-    v = np.random.normal(0, 1, dimension)  # Generate a vector with normally distributed components
-    v /= np.linalg.norm(v)  # Normalize the vector to make it a unit vector
-    r = np.random.uniform(0, 1) ** (1.0 / dimension)  # Scale factor to ensure uniform distribution in the sphere
-    return v * r  # Scale the unit vector to a random point within the unit sphere
-
-
-def jitter_ia(structure, ia=0.01, seed = None):
+def jitter_ia(structure, ia=0.01, seed=None):
     """
     jitter atoms, based on cellcvrt.c++ by atat
 
@@ -24,6 +27,7 @@ def jitter_ia(structure, ia=0.01, seed = None):
     structure.set_positions(structure.positions + translation)
     return structure
 
+
 def jitter_ic(structure, ic=0.005, seed=None):
     """
     jitter cell, based on cellcvrt.c++ by atat
@@ -31,30 +35,18 @@ def jitter_ic(structure, ic=0.005, seed=None):
     Parameters:
     structure: ase atom structure.
     ic: maximum amount of jitter in Angstroms.
-    if (jittercell>0.) {
-      for (int i=0; i<3; i++) {
-    	rVector3d v=rnd_ball()*jittercell;
-    	for (int j=0; j<3; j++) {
-    	  transfo(j,i)+=v(j);
-        	}
-          }
-        }
-    lat.cell=transfo*lat.cell;
-    axes=transfo*axes;
-    for (int i=0; i<lat.atom_pos.get_size(); i++) {
-      lat.atom_pos(i)=transfo*lat.atom_pos(i);
-    }
     """
     if seed is not None:
         np.random.seed(seed)
     transform = np.identity(3) + ic*np.array([rnd_ball() for _ in range(3)])
-    structure.set_cell(transform@structure.get_cell(), scale_atoms= True)
+    structure.set_cell(transform@structure.get_cell(), scale_atoms=True)
     return structure
 
+
 def compute_distortion(before, after, quiet=True):
-    if type(before) == ase.atoms.Atoms:
+    if type(before) is ase.atoms.Atoms:
         before = before.cell.array
-    if type(after) == ase.atoms.Atoms:
+    if type(after) is ase.atoms.Atoms:
         after = after.cell.array
         
     # Normalize the matrices by their cubic root of the determinant
--- disalloy/grid_run.py
+++ disalloy/grid_run.py
@@ -16,17 +16,21 @@ def write_log(message, log_file='application.log'):
     with open(log_file, 'a') as file:
         file.write(log_entry)
 
+
 ase_to_pmg = pg_ase.AseAtomsAdaptor.get_structure
 pmg_to_ase = pg_ase.AseAtomsAdaptor.get_atoms
 
 
-def run_packet(df_name, df_global, relaxer_dict, size_of_packet, structure_size, copy_calculated = None):
+def run_packet(df_name, df_global, relaxer_dict, size_of_packet,
+               structure_size,
+               copy_calculated=None):
     write_log('starting packet run: attempting {} runs of size {}'.format(
         str(size_of_packet), str(structure_size)),
               log_file=df_name+'.log')
-    calculator_label, relaxer = relaxer_dict['calculator_label'], relaxer_dict['relaxer']
-    df = df_global #.copy()
-    if not calculator_label in df.columns:
+    calculator_label, relaxer = relaxer_dict['calculator_label'], relaxer_dict[
+        'relaxer']
+    df = df_global
+    if calculator_label not in df.columns:
         df[calculator_label] = None
         df[calculator_label] = df[calculator_label].astype(object)
         df.attrs[calculator_label] = {}
@@ -35,28 +39,29 @@ def run_packet(df_name, df_global, relaxer_dict, size_of_packet, structure_size,
     else:
         uncalculated = df.attrs[calculator_label]['uncalculated']
     
-
-    
     uncalculated_size = list(
         set(df[df['size'] == structure_size].index.to_list()) & 
         set(uncalculated)) 
 
     if copy_calculated is not None:
         if copy_calculated not in df.columns:
-            write_log('this relaxer has not been applied yet and cannot be copied therefore',
-              log_file=df_name+'.log')
+            write_log('this relaxer has not been applied yet and cannot be'
+                      ' copied therefore',
+                      log_file=df_name+'.log')
             return
         if copy_calculated == calculator_label:
             write_log('same relaxer selected for copy,'
-                'please select another relaxer calculated index, or appply a new relaxer',
-              log_file=df_name+'.log')
+                      'please select another relaxer calculated index, or'
+                      ' apply a new relaxer',
+                      log_file=df_name+'.log')
             return
-        uncalculated_size = list( set(uncalculated_size) & 
+        uncalculated_size = list(set(uncalculated_size) & 
                                  set(df.attrs[copy_calculated]['calculated']))
         
     rest_number = len(uncalculated_size)
     if rest_number == 0:
-        write_log('no more structures to run of this size', log_file=df_name+'.log')
+        write_log('no more structures to run of this size', 
+                  log_file=df_name+'.log')
         return
     if rest_number > size_of_packet:
         to_calculate = random.sample(uncalculated_size, size_of_packet)
@@ -67,11 +72,13 @@ def run_packet(df_name, df_global, relaxer_dict, size_of_packet, structure_size,
         relaxer, axis=1)
     ti = time.time() - ti
 
-    df.attrs[calculator_label]['uncalculated'] = list(set(uncalculated) - set(to_calculate))
+    df.attrs[calculator_label]['uncalculated'] = list(
+        set(uncalculated) - set(to_calculate))
     df.attrs[calculator_label]['calculated'] += to_calculate
 
     df.to_pickle(df_name+'.pkl')
     write_log('index of ran strctures:\n {}'.format(str(to_calculate)),
               log_file=df_name+'.log')
-    write_log('Succesfuly ran packet of {} runs in {}s'.format(len(to_calculate), ti),
+    write_log('Succesfuly ran packet of {} runs in {}s'.format(
+        len(to_calculate), ti),
               log_file=df_name+'.log')
